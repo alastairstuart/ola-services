865a1b902670c88aa606fe0fb159ed96f5c6ae1e
Al Stuart <alastair.stuart@gmail.com>
2023-11-11 10:51:16 +0000

    Add date simulation to the simulated requests

--- simulateClient.sh
+++ simulateClient.sh
@@ -9,8 +9,25 @@ fi
 # Base URL provided as the first argument
 BASE_URL=$1
 
+# Get Unix timestamp (seconds since the epoch)
+SECONDS=$(date +%s)
+
+# Get current nanoseconds and extract milliseconds
+NANOSECONDS=$(date +%N)
+MILLISECONDS=$(echo $NANOSECONDS | cut -c1-3)
+
+# Combine seconds and milliseconds
+CURRENT_TIMESTAMP="${SECONDS}${MILLISECONDS}"
+
+# Get current hour of the day
+CURRENT_HOUR=$(date +%H)
+
+# Get current numerical day of the week (1=Monday, 7=Sunday)
+CURRENT_DOW=$(date +%u)
+
 # Path and query string
-PATH_QUERY="/i?app_key=aee33f197f73bc5f7ca99261a6f253caa2d4f614&timestamp=1699696583746&hour=9&dow=6&tz=0&sdk_version=20.04&sdk_name=java-native-android&user_details=%7B%22custom%22%3A%7B%22device_id%22%3A%22CQ8D%22%2C%22ota%22%3A%221%22%7D%7D&device_id=79b5c513422783f4&checksum=7c8e0f1ebfdd4d9164ac9f53ae2f243d9de6a9dc"
+# Checksum will not be valid but sufficient for current purposes
+PATH_QUERY="/i?app_key=aee33f197f73bc5f7ca99261a6f253caa2d4f614&timestamp=${CURRENT_TIMESTAMP}&hour=${CURRENT_HOUR}&dow=${CURRENT_DOW}&tz=0&sdk_version=20.04&sdk_name=java-native-android&user_details=%7B%22custom%22%3A%7B%22device_id%22%3A%22CQ8D%22%2C%22ota%22%3A%221%22%7D%7D&device_id=79b5c513422783f4&checksum=7c8e0f1ebfdd4d9164ac9f53ae2f243d9de6a9dc"
 
 # User agent string
 USER_AGENT="Dalvik/2.1.0 (Linux; U; Android 14; sdk_gphone64_arm64 Build/UE1A.230829.030)"
